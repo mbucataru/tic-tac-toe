a95eca69de320d2e257ef9e9fb0f6e09b29db1db
mbucataru <mbucatarugithub@gmail.com>
2023-08-14 21:46:34 +0300

    Finalize game logic and interface

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,21 +1,23 @@
 # Contains the logic for various states the Game class needs to check for
 module Checks
-  def over?(grid)
+  def over?(grid, empty_squares)
+    return true if empty_squares.zero?
+
     3.times do |current|
-      unless grid[current][0] == 'B'
+      unless grid[current][0] == ' '
         return true if grid[current][0] == grid[current][1] && grid[current][0] == grid[current][2]
       end
 
-      unless grid[0][current] == 'B'
+      unless grid[0][current] == ' '
         return true if grid[0][current] == grid[1][current] && grid[0][current] == grid[2][current]
       end
     end
 
-    unless grid[0][0] == 'B'
+    unless grid[0][0] == ' '
       return true if grid[0][0] == grid[1][1] && grid[0][0] == grid[2][2]
     end
 
-    unless grid[0][2] == 'B'
+    unless grid[0][2] == ' '
       return true if grid[0][2] == grid[1][1] && grid[0][2] == grid[2][0]
     end
 
@@ -23,7 +25,7 @@ module Checks
   end
 
   def move_valid?(move_row, move_column, grid)
-    if move_row.negative? || move_row > 2 || move_column.negative? || move_column > 2 || grid[move_row][move_column] != 'B'
+    if move_row.negative? || move_row > 2 || move_column.negative? || move_column > 2 || grid[move_row][move_column] != ' '
       return false
     end
 
@@ -34,10 +36,12 @@ end
 # Contains the logic for building and augmenting the board in the game
 class Board
   def initialize
-    @grid = Array.new(3) { Array.new(3, 'B') }
+    @grid = Array.new(3) { Array.new(3, ' ') }
+    @empty_squares = 9
   end
 
   attr_reader :grid
+  attr_accessor :empty_squares
 
   def to_s
     @grid.map { |row| row.join(' | ') }.join("\n#{'-' * 10}\n")
@@ -45,6 +49,7 @@ class Board
 
   def make_move(move_row, move_column, player)
     grid[move_row][move_column] = player
+    self.empty_squares -= 1
   end
 end
 
@@ -68,7 +73,8 @@ class Game
       board.make_move(move_row, move_column, player)
       self.player = player == 'X' ? 'O' : 'X'
     else
-      false
+      puts 'Invalid move'
+      sleep(2)
     end
   end
 end
@@ -79,17 +85,21 @@ class TicTacToe
 
   def self.play
     game = Game.new
-    until game.over?(game.board.grid)
+    until game.over?(game.board.grid, game.board.empty_squares)
       puts game.board
       puts "It is Player #{game.player}'s turn: "
       user_move = gets.chomp
       game.move(user_move)
     end
     puts game.board
-    if game.player == 'X'
-      puts 'Player O has won the game!'
+    if game.board.empty_squares.zero?
+      puts 'It was a draw!'
     else
-      puts 'Player X has won the game!'
+      if game.player == 'X'
+        puts 'Player O has won the game!'
+      else
+        puts 'Player X has won the game!'
+      end
     end
   end
 end
